915c663231ad7f5f830b603799a693acf6325315
Nguyen Minh Phuong <phuongnm.bkhn@gmail.com>
2019-12-08 08:54:17 +0900

    update brnn

--- onmt/encoders/rnn_self_att_encoder.py
+++ onmt/encoders/rnn_self_att_encoder.py
@@ -6,7 +6,6 @@ from torch.nn.utils.rnn import pad_packed_sequence as unpack
 from onmt.encoders.rnn_encoder import RNNEncoder
 from onmt.modules import MultiHeadedAttention
 from onmt.utils.misc import sequence_mask
-from onmt.utils.rnn_factory import rnn_factory
 
 
 class RNNSelfAttentionEncoder(RNNEncoder):
@@ -26,35 +25,14 @@ class RNNSelfAttentionEncoder(RNNEncoder):
                  hidden_size, dropout=0.0, embeddings=None,
                  use_bridge=False):
         super(RNNSelfAttentionEncoder, self).__init__(rnn_type, bidirectional, num_layers,
-                                                      hidden_size, dropout=0.0, embeddings=embeddings,
-                                                      use_bridge=False)
-        assert embeddings is not None
-
-        num_directions = 2 if bidirectional else 1
-        assert hidden_size % num_directions == 0
-        hidden_size = hidden_size // num_directions
-        self.embeddings = embeddings
+                                                      hidden_size, dropout=dropout, embeddings=embeddings,
+                                                      use_bridge=use_bridge)
 
         self.layer_norm = nn.LayerNorm(hidden_size, eps=1e-6)
 
         self.self_attn = MultiHeadedAttention(
             head_count=8, model_dim=hidden_size, dropout=dropout,
             max_relative_positions=0)
-
-        self.rnn, self.no_pack_padded_seq = \
-            rnn_factory(rnn_type,
-                        input_size=embeddings.embedding_size,
-                        hidden_size=hidden_size,
-                        num_layers=num_layers,
-                        dropout=dropout,
-                        bidirectional=bidirectional)
-
-        # Initialize the bridge layer
-        self.use_bridge = use_bridge
-        if self.use_bridge:
-            self._initialize_bridge(rnn_type,
-                                    hidden_size,
-                                    num_layers)
 
     def forward(self, src, lengths=None):
         """See :func:`EncoderBase.forward()`"""
